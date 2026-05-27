fe9424598f934be0b08c8758449f2216512f70f8
gabydonato <gabriellysantosdesouza64@gmail.com>
2026-05-27 11:24:42 -0300

    Add files via upload

---
+++ hamburgueria/TODO.md
@@ -0,0 +1,11 @@
+# TODO
+
+- [x] Ajustar `index.html` para agrupar formulário e iframe do contato em um mesmo layout (div wrapper).
+
+- [x] Atualizar `css/style.css` criando um grid para `#contato` com 2 colunas no desktop e 1 coluna no mobile.
+
+- [x] Validar visualmente no navegador (desktop e mobile) para garantir centralização do rodapé do mapa.
+
+
+
+
---
+++ hamburgueria/js/dados.js
@@ -0,0 +1,15 @@
+// Dados do cardápio/serviços do cliente.
+// Edite este arquivo para alterar os itens exibidos no site.
+const ITENS = [
+  { nome: "Duplo Cheddar Bacon", descricao: "Descrição breve do item.", preco: "R$ 19,90", imagem: "img/chedbacon.png" },
+  { nome: "Smash Duplo", descricao: "Descrição breve do item.", preco: "R$ 19,90", imagem: "img/smashduplo.png" },
+  { nome: "Veggie Burger", descricao: "Descrição breve do item.", preco: "R$ 27,90", imagem: "img/veggieburguer.jpg" },
+  { nome: "Batata média",tamanho: "Média", preco: "R$ 14,90", imagem: "img/batatamedia.jpg" },
+  { nome: "Porção Batata Rústica",tamanho: "Grande", preco: "R$ 19,90", imagem: "img/batatarustica.jpg" },
+  { nome: "Baldão",tamanho: "Baldão", preco: "R$ 24,90", imagem: "img/baldao.jpg" },
+  { nome: "Refrigerantes", preco: "R$ 12,90", imagem: "img/coca.png" },
+  { nome: "Refrigerantes", preco: "R$10,00", imagem: "img/refri.png" },
+  { nome: "Sucos naturais", preco: "R$ 10,50", imagem: "img/sucos.png" },
+  { nome: "", preco: "R$ 14,90", imagem: "img/succ.png" },
+  { nome: "Combo família", preco: "R$ 189,90", imagem: "img/combo.png" },
+];
