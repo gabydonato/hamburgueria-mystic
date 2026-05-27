88a2b8c4bcf036184769050ca57925ac165ca2c5
gabydonato <gabriellysantosdesouza64@gmail.com>
2026-05-27 11:54:53 -0300

    Update dados.js

--- hamburgueria/js/dados.js
+++ hamburgueria/js/dados.js
@@ -3,13 +3,12 @@
 const ITENS = [
   { nome: "Duplo Cheddar Bacon", preco: "R$ 19,90", imagem: "img/chedbacon.png" },
   { nome: "Smash Duplo", preco: "R$ 19,90", imagem: "img/smashduplo.png" },
-  { nome: "Veggie Burger", descricao: "Hambúrguer vegano.", preco: "R$ 27,90", imagem: "img/veggieburguer.jpg" },
+  { nome: "Veggie Burger", preco: "R$ 27,90", imagem: "img/veggieburguer.jpg" },
   { nome: "Batata média",tamanho: "Média", preco: "R$ 14,90", imagem: "img/batatamedia.jpg" },
   { nome: "Porção Batata Rústica",tamanho: "Grande", preco: "R$ 19,90", imagem: "img/batatarustica.jpg" },
   { nome: "Baldão",tamanho: "Baldão", preco: "R$ 24,90", imagem: "img/baldao.jpg" },
-  { nome: "Refrigerantes", preco: "R$ 12,90", imagem: "img/coca.png" },
+  { nome: "Coca-cola", preco: "R$ 12,90", imagem: "img/coca.png" },
   { nome: "Refrigerantes", preco: "R$10,00", imagem: "img/refri.png" },
   { nome: "Sucos naturais", preco: "R$ 10,50", imagem: "img/sucos.png" },
-  { nome: "", preco: "R$ 14,90", imagem: "img/succ.png" },
   { nome: "Combo família", preco: "R$ 189,90", imagem: "img/combo.png" },
 ];
